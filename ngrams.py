b601c1fc7c6bfd37a960d693f66ce0b90c380e4b
KellanChilders <kellanchilders@gmail.com>
2017-10-09 16:28:37 -0400

    Tidied repo

--- ngrams.py
+++
@@ -1,9 +0,0 @@
-def get_ngrams(sentence, n):
-    sentence = sentence.split()
-    return [sentence[i:i+n] for i in range(len(sentence)+1 - n)]
-
-if __name__ == "__main__":
-    print(get_ngrams("This was a triumph", 1))
-    print(get_ngrams("I'm writing a note here, huge success", 2))
-    print(get_ngrams("It's hard to overstate my satisfaction", 3))
-    # Sadly nltk does it just as well, so this will never get used.
